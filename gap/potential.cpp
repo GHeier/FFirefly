60c01a821e24203e993dca9cb66b35509a3602c9
Gsnores <griffinheier@gmail.com>
2024-06-09 16:17:24 -0700

    q=0 works too

--- gap/potential.cpp
+++ gap/potential.cpp
@@ -222,8 +222,7 @@ double integrate_susceptibility(Vec q, double T, double mu, double w, int num_po
     //return imaginary_integration(q, T, mu, w, num_points, 0.000);
     if (q.vals.norm() < 0.0001) {
         vector<Vec> FS = tetrahedron_method(e_base_avg, q, mu);
-        double sum = 0; for (auto v : FS) sum += v.area / vp(v);
-        return sum / pow(2*k_max,dim);
+        return get_DOS(FS);
         return tetrahedron_sum(e_surface, vp_surface, q, 0, w, T);
         double c, d; get_bounds3(q, d, c, e_surface);
         vector<double> spacing; get_spacing_vec(spacing, w, c, d, num_points);
