32ea76676b714d0912dff1bfc2177f808f35fd83
Gsnores <griffinheier@gmail.com>
2024-12-06 20:16:08 -0800

    Finished fortran; Tests to follow

--- src/config/load/fortran_config.f90
+++ src/config/load/fortran_config.f90
@@ -2,107 +2,128 @@ module confighub
     use iso_c_binding
     implicit none
     ! Global variables
-    integer(c_int), bind(C, name="c_ibrav") :: c_ibrav 
-    integer(c_int), bind(C, name="c_k_mesh") :: c_k_mesh(3)
-    integer(c_int), bind(C, name="c_q_mesh") :: c_q_mesh(3)
-    integer(c_int), bind(C, name="c_w_pts") :: c_w_pts
-    integer(c_int), bind(C, name="c_dim") :: c_dim
 
-    real(c_float), bind(C, name="c_fermi_energy") :: c_fermi_energy
-    real(c_float), bind(C, name="c_onsite_U") :: c_onsite_U
-    real(c_float), bind(C, name="c_bcs_cutoff_frequency") :: c_bcs_cutoff_frequency
-    real(c_float), bind(C, name="c_cell") :: c_cell(3,3)
-    real(c_float), bind(C, name="c_brillouin_zone") :: c_brillouin_zone(3,3)
-    real(c_float), bind(C, name="c_max_freq") :: c_max_freq
-
+    ![CONTROL]
     character(kind=c_char), bind(C, name="c_category") :: c_category(50)
-    character(kind=c_char), bind(C, name="c_outdir") :: c_outdir(50)
-    character(kind=c_char), bind(C, name="c_prefix") :: c_prefix(50)
-    character(kind=c_char), bind(C, name="c_verbosity") :: c_verbosity(50)
-    character(kind=c_char), bind(C, name="c_interaction") :: c_interaction(50)
+    character(len=50) :: category
     character(kind=c_char), bind(C, name="c_calculation") :: c_calculation(50)
+    character(len=50) :: calculation
+    character(kind=c_char), bind(C, name="c_outdir") :: c_outdir(50)
+    character(len=50) :: outdir
+    character(kind=c_char), bind(C, name="c_prefix") :: c_prefix(50)
+    character(len=50) :: prefix
+    character(kind=c_char), bind(C, name="c_verbosity") :: c_verbosity(50)
+    character(len=50) :: verbosity
+    character(kind=c_char), bind(C, name="c_datfile_in") :: c_datfile_in(50)
+    character(len=50) :: datfile_in
+    character(kind=c_char), bind(C, name="c_datfile_out") :: c_datfile_out(50)
+    character(len=50) :: datfile_out
 
-    integer :: ibrav, k_mesh(3), q_mesh(3), w_pts, dim
-    real :: fermi_energy, onsite_U, bcs_cutoff_frequency, cell(3,3), brillouin_zone(3,3), max_freq
-    character(len=50) :: category, calculation, outdir, prefix, verbosity, interaction
+    ![SYSTEM]
+    character(kind=c_char), bind(C, name="c_interaction") :: c_interaction(50)
+    character(len=50) :: interaction
+    integer(c_int), bind(C, name="c_dimension") :: c_dimension
+    integer :: dimension
+    integer(c_int), bind(C, name="c_ibrav") :: c_ibrav
+    integer :: ibrav
+    real(c_float), bind(C, name="c_fermi_energy") :: c_fermi_energy
+    real :: fermi_energy
+    real(c_float), bind(C, name="c_onsite_U") :: c_onsite_U
+    real :: onsite_U
+
+    ![MESH]
+    integer(c_int), bind(C, name="c_k_mesh") :: c_k_mesh(3)
+    integer :: k_mesh(3)
+    integer(c_int), bind(C, name="c_q_mesh") :: c_q_mesh(3)
+    integer :: q_mesh(3)
+    integer(c_int), bind(C, name="c_w_pts") :: c_w_pts
+    integer :: w_pts
+
+    ![CELL]
+    real(c_float), bind(C, name="c_cell") :: c_cell(3,3)
+    real :: cell(3,3)
+    real(c_float), bind(C, name="c_brillouin_zone") :: c_brillouin_zone(3,3)
+    real :: brillouin_zone(3,3)
+
+    ![BANDS]
+    character(kind=c_char), bind(C, name="c_bands") :: c_bands(50)
+    character(len=50) :: bands
+    real(c_float), bind(C, name="c_eff_mass") :: c_eff_mass
+    real :: eff_mass
+
+    ![SUPERCONDUCTOR]
+    logical(c_bool), bind(C, name="c_FS_only") :: c_FS_only
+    logical :: FS_only
+    real(c_float), bind(C, name="c_bcs_cutoff_frequency") :: c_bcs_cutoff_frequency
+    real :: bcs_cutoff_frequency
+    integer(c_int), bind(C, name="c_num_eigenvalues_to_save") :: c_num_eigenvalues_to_save
+    integer :: num_eigenvalues_to_save
+    integer(c_int), bind(C, name="c_frequency_pts") :: c_frequency_pts
+    integer :: frequency_pts
+
+    ![RESPONSE]
+    logical(c_bool), bind(C, name="c_dynamic") :: c_dynamic
+    logical :: dynamic
     ! End of global variables
 
-    ! Global functions
     interface
-        function get_calculation() bind(C)
-            use iso_c_binding
-            type(c_ptr) :: get_calculation
-        end function get_calculation
+    ! Global functions
 
+    ![CONTROL]
         function get_category() bind(C)
             use iso_c_binding
             type(c_ptr) :: get_category
-        end function get_category
+            end function get_category
+
+        function get_calculation() bind(C)
+            use iso_c_binding
+            type(c_ptr) :: get_calculation
+            end function get_calculation
 
         function get_outdir() bind(C)
             use iso_c_binding
             type(c_ptr) :: get_outdir
-        end function get_outdir
+            end function get_outdir
 
         function get_prefix() bind(C)
             use iso_c_binding
             type(c_ptr) :: get_prefix
-        end function get_prefix
+            end function get_prefix
 
         function get_verbosity() bind(C)
             use iso_c_binding
             type(c_ptr) :: get_verbosity
-        end function get_verbosity
+            end function get_verbosity
 
+        function get_datfile_in() bind(C)
+            use iso_c_binding
+            type(c_ptr) :: get_datfile_in
+            end function get_datfile_in
+
+        function get_datfile_out() bind(C)
+            use iso_c_binding
+            type(c_ptr) :: get_datfile_out
+            end function get_datfile_out
+
+
+    ![SYSTEM]
         function get_interaction() bind(C)
             use iso_c_binding
             type(c_ptr) :: get_interaction
-        end function get_interaction
-        ! End of global functions
+            end function get_interaction
 
-    end interface
 
-contains
+    ![MESH]
 
-    function get_string(c_string) result(fortran_string)
-        type(c_ptr), intent(in) :: c_string
-        character(len=:), allocatable :: fortran_string
-        character(kind=c_char, len=1), pointer :: c_string_ptr(:)
-        integer :: i, length
-        call c_f_pointer(c_string, c_string_ptr, [1000])  ! 1000 is a safe buffer size; adjust as needed
-        length = 0
-        do i = 1, size(c_string_ptr)
-            if (c_string_ptr(i) == c_null_char) exit
-            length = length + 1
-        end do
-        allocate(character(len=length) :: fortran_string)
-        do i = 1, length
-            fortran_string(i:i) = c_string_ptr(i)
-        end do
-    end function get_string
+    ![CELL]
 
-    subroutine load_f90_config()
-        ! Load variables
-        ibrav = c_ibrav
-        w_pts = c_w_pts
-        dim = c_dim
-        k_mesh = c_k_mesh
-        q_mesh = c_q_mesh
+    ![BANDS]
+        function get_bands() bind(C)
+            use iso_c_binding
+            type(c_ptr) :: get_bands
+            end function get_bands
 
-        fermi_energy = c_fermi_energy
-        onsite_U = c_onsite_U
-        bcs_cutoff_frequency = c_bcs_cutoff_frequency
-        cell = c_cell
-        brillouin_zone = c_brillouin_zone
-        max_freq = c_max_freq
 
-        calculation = get_string(get_calculation())
-        category = get_string(get_category())
-        outdir = get_string(get_outdir())
-        prefix = get_string(get_prefix())
-        verbosity = get_string(get_verbosity())
-        interaction = get_string(get_interaction())
-        ! End of loading variables
-    end subroutine load_f90_config
+    ![SUPERCONDUCTOR]
 
-end module confighub
+    ![RESPONSE]
---
+++ src/config/write/write_f90.py
@@ -0,0 +1,114 @@
+from write_c import remove_lines_between_phrases, add_lines_between_phrases
+start_phrase = '    ! Global variables'
+end_phrase = '    ! End of global variables'
+start_func_phrase = '    ! Global functions'
+end_func_phrase = '    ! End of Global Functions'
+start_load_phrase = '        ! Load variables'
+end_load_phrase = '        ! End of loading variables'
+
+
+def format_var_line(key, value):
+    if (type(value) == str):
+        return f"    character(kind=c_char), bind(C, name=\"c_{key}\") :: c_{key}(50)\n    character(len=50) :: {key}"
+    elif (type(value) == int):
+        return f"    integer(c_int), bind(C, name=\"c_{key}\") :: c_{key}\n    integer :: {key}"
+    elif (type(value) == float):
+        return f"    real(c_float), bind(C, name=\"c_{key}\") :: c_{key}\n    real :: {key}"
+    elif (type(value) == bool):
+        return f"    logical(c_bool), bind(C, name=\"c_{key}\") :: c_{key}\n    logical :: {key}"
+    elif (type(value) == list):
+        if (type(value[0]) == int):
+            return f"    integer(c_int), bind(C, name=\"c_{key}\") :: c_{key}(3)\n    integer :: {key}(3)"
+        elif (type(value[0]) == float):
+            return f"    real(c_float), bind(C, name=\"c_{key}\") :: c_{key}(3)\n    real :: {key}(3)"
+        elif (type(value[0]) == list):
+            if (type(value[0][0]) == int):
+                return f"    integer(c_int), bind(C, name=\"c_{key}\") :: c_{key}(3,3)\n    integer :: {key}(3,3)"
+            elif (type(value[0][0]) == float):
+                return f"    real(c_float), bind(C, name=\"c_{key}\") :: c_{key}(3,3)\n    real :: {key}(3,3)"
+        else:
+            print("Error: Unsupported type in config file (list section)")
+            exit(1)
+
+def format_func_line(key, value):
+    return f"        function get_{key}() bind(C)\n            use iso_c_binding\n            type(c_ptr) :: get_{key}\n            end function get_{key}\n"
+
+def format_load_line(key, value):
+    if (type(value) == str):
+        return f"            {key} = get_string(get_{key}())"
+    else:
+        return f"            {key} = c_{key}"
+
+def get_new_func_lines(ALL):
+    new_lines = []
+    for section in ALL:
+        new_lines.append('\n    !' + '[' + section + ']\n')
+        for key, value in ALL[section].items():
+            if (section == "CELL" or type(value) != str): 
+                continue
+            new_lines.append(format_func_line(key, value) + '\n')
+        new_lines.append('')
+    return new_lines
+
+def get_new_load_lines(ALL):
+    new_lines = []
+    for section in ALL:
+        new_lines.append('\n!' + '[' + section + ']\n')
+        for key, value in ALL[section].items():
+            new_lines.append(format_load_line(key, value) + '\n')
+        new_lines.append('')
+    return new_lines
+
+def get_new_var_lines(ALL):
+    new_lines = []
+    for section in ALL:
+        new_lines.append('\n    !' + '[' + section + ']\n')
+        for key, value in ALL[section].items():
+            new_lines.append(format_var_line(key, value) + '\n')
+        new_lines.append('')
+    return new_lines
+
+def add_lines_to_vars(ALL, file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    lines = remove_lines_between_phrases(lines, start_phrase, end_phrase)
+    new_lines = get_new_var_lines(ALL)
+    updated_lines = add_lines_between_phrases(lines, new_lines, start_phrase, end_phrase)
+    
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(updated_lines)
+
+def add_lines_to_funcs(ALL, file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    lines = remove_lines_between_phrases(lines, start_func_phrase, end_func_phrase)
+    new_lines = get_new_func_lines(ALL)
+    updated_lines = add_lines_between_phrases(lines, new_lines, start_func_phrase, end_func_phrase)
+    
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(updated_lines)
+
+def add_lines_to_load(ALL, file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    lines = remove_lines_between_phrases(lines, start_load_phrase, end_load_phrase)
+    new_lines = get_new_load_lines(ALL)
+    updated_lines = add_lines_between_phrases(lines, new_lines, start_load_phrase, end_load_phrase)
+    
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(updated_lines)
+
+def write_f90(ALL):
+    file_path = 'load/fortran_config.f90'
+    add_lines_to_vars(ALL, file_path)
+    add_lines_to_funcs(ALL, file_path)
+    add_lines_to_load(ALL, file_path)
+
+    print(f"Successfully updated the file '{file_path}'.")
+
