d54c37bd04adcb13a0a2856a31acc9ec72354ba9
Gsnores <griffinheier@gmail.com>
2025-02-20 18:45:27 -0800

    Added band and vertex classes

--- fcode/objects/CondensedMatterField/CondensedMatterField/src/CondensedMatterField.jl
+++
@@ -1,5 +0,0 @@
-module CondensedMatterField
-
-greet() = print("Hello World!")
-
-end # module CondensedMatterField
--- fcode/objects/CondensedMatterField/precompile.jl
+++
@@ -1,4 +0,0 @@
-using CondensedMatterField
-cmf = CondensedMatterField.CMF("data.txt")  # Precompile by running a sample function
-result = cmf([1.0, 2.0, 3.0], 0.5)
-
