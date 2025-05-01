c630c4fad2144b47d03c8a4e802200c5a6090af9
Griffin Heier <griffinheier@gmail.com>
2025-05-01 01:11:08 -0700

    Module write making good progress

--- src/module/write/write_export.py
+++ src/module/write/write_export.py
@@ -1,6 +1,8 @@
 from . import base
+import copy
 
 letters = "abcdefghijklmnopqrstu"
+variables = {"Vec": "v"}
 
 
 def write_export_include(INPUTS, lines):
@@ -24,31 +26,70 @@ def arg_cpp_to_c(arg, name):
     return arg
 
 
-def type_convert_lines(args):
+def type_convert_lines(a, i):
     lines = []
-    for i in range(len(args)):
-        a = args[i]
-        if a == "Vec":
-            lines.append("Vec v(" + letters[i] + ", " + letters[i + 1] + ");")
-        if a == "vector":
-            lines.append("Vec v(" + letters[i] + ", " + letters[i + 1] + ");")
+    if a == "Vec":
+        lines.append(
+            "    Vec "
+            + variables["Vec"]
+            + "("
+            + letters[i - 1]
+            + ", "
+            + letters[i]
+            + ");"
+        )
+    if a == "vector":
+        lines.append(
+            "    Vec "
+            + variables["Vec"]
+            + "("
+            + letters[i - 1]
+            + ", "
+            + letters[i]
+            + ");"
+        )
+    return lines
 
 
 def create_declaration(name, args, num):
+    arguments = copy.copy(args)
     line = 'extern "C" '
-    a = arg_cpp_to_c(args[0], name)
+    a = arg_cpp_to_c(arguments[0], name)
     line += a + " "
     line += name + "_export" + str(num)
     line += "("
-    for i in range(1, len(args)):
-        a = arg_cpp_to_c(args[i], name)
+    i = 1
+    while i < len(arguments):
+        a = arg_cpp_to_c(arguments[i], name)
         if len(a) == 2:
-            args.insert(i, a[1])
+            del arguments[i]
+            arguments.insert(i, a[0])
+            arguments.insert(i + 1, a[1])
+            a = a[0]
         line += a + " " + letters[i - 1]
-        if i != len(args) - 1:
+        if i != len(arguments) - 1:
             line += ", "
+        i += 1
     line += ") {"
     return line
+
+
+def create_func(name, args, num):
+    lines = []
+
+    line = "    return " + name + "("
+    for i in range(1, len(args)):
+        newline = type_convert_lines(args[i], i)
+        lines.extend(newline)
+        if len(newline) == 0:
+            line += letters[i - 1]
+        else:
+            line += variables[args[i]]
+        if i != len(args) - 1:
+            line += ", "
+    line += ");\n}"
+    lines.append(line)
+    return lines
 
 
 def write_export_func(INPUTS, lines):
@@ -56,7 +97,10 @@ def write_export_func(INPUTS, lines):
         i = 0
         prev_name = ""
         for name, args in func.items():
-            line = create_declaration(name, args, i)
+            line = create_declaration(name, args, i) + "\n"
+            func_lines = create_func(name, args, i)
+            for l in func_lines:
+                line += l + "\n"
             if prev_name == name:
                 i += 1
             else:
@@ -68,6 +112,6 @@ def write_export_func(INPUTS, lines):
 
 
 def write_export(INPUTS):
-    with open("../exports/cpp_export.cpp", "r") as file:
+    with open("exports/cpp_export.cpp", "r") as file:
         lines = file.readlines()
     write_export_func(INPUTS, lines)
